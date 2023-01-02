2215cae62726126eb49c4fd0bc87200b12cadea6
aanurin <aanurin@griddynamics.com>
2023-01-02 15:13:41 +0100

    branch2 init

--- finished_code/src/main/java/com/example/android/kotlincoroutines/GitTest2.kt
+++ finished_code/src/main/java/com/example/android/kotlincoroutines/GitTest2.kt
@@ -2,4 +2,9 @@ package com.example.android.kotlincoroutines
 
 class GitTest2 {
     //! branch2 init
+    //! branch1 init
+    //! branch1 commit 2. //! commit 3
+
+    //! add 1
+    //! add 2
 }
